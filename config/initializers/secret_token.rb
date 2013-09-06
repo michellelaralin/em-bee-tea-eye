4a3ef1d11ca2ac84b03fb35264cc08267a0e0cda
Michelle Lara Lin <michelle@michellelaralin.com>
2013-09-05 21:10:14 -0400

    shit yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MBTI::Application.config.secret_token = '37a262914865937fd5a015544c1bccffea06365f5998245600c6022b530f2cc7f761f022e2a370c7e1073e4a09232e413567573ab278299b47326a2853512f83'
